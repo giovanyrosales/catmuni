013bedf11fef88f1b33a60faebea8add76a2f70e
Thiago <thiagomancia@gmail.com>
2021-12-09 14:00:57 -0600

    Modificación de Sidebar y form agregar empresa

---
+++ app/Http/Controllers/Backend/Contribuyentes/ContribuyentesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Contribuyentes;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use function PHPUnit\Framework\isEmpty;
+
+
+
+class ContribuyentesController extends Controller
+{
+    //
+}
--- resources/views/backend/menus/sidebar.blade.php
+++ resources/views/backend/menus/sidebar.blade.php
@@ -9,13 +9,13 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
-
+    <!-- Grupo Roles y permisos -->
              <li class="nav-item">
 
                  <a href="#" class="nav-link">
                     <i class="far fa-edit"></i>
                     <p>
-                        Roles y Permisos
+                        Config
                         <i class="fas fa-angle-left right"></i>
                     </p>
                 </a>
@@ -23,40 +23,64 @@
                 <ul class="nav nav-treeview">
                     <li class="nav-item">
                         <a href="{{ route('admin.roles.index') }}" target="frameprincipal" class="nav-link">
-                            <i class="fas fa-user-plus nav-icon"></i>
+                            <i class="fas fa-user-friends nav-icon"></i>
                             <p>Roles</p>
                         </a>
                     </li>
 
                     <li class="nav-item">
                         <a href="{{ route('admin.permisos.index') }}" target="frameprincipal" class="nav-link">
-                            <i class="far fa-circle nav-icon"></i>
-                            <p>Permisos</p>
+                            <i class="fas fa-user-shield nav-icon"></i>
+                            <p>Permisos y Usuarios</p>
                         </a>
                     </li>
-
                 </ul>
              </li>
-
-                <li class="nav-item">
-
-                    <a href="#" class="nav-link">
-                        <i class="far fa-edit"></i>
-                        <p>
-                            Config
-                            <i class="fas fa-angle-left right"></i>
-                        </p>
-                    </a>
-
-                    <ul class="nav nav-treeview">
-
-                        <li class="nav-item">
-                            <a href="{{ route('admin.crear.empresa.index') }}" target="frameprincipal" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Empresas</p>
-                            </a>
-                        </li>
-
+            
+            <!-- Grupo Empresas -->
+                                <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="fas fa-building"></i>
+                                    <p>Empresas 
+                                    <i class="right fas fa-angle-left"></i>
+                                    </p>
+                                </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                <a href="{{ route('admin.crear.empresa.index') }}" target="frameprincipal" class="nav-link">
+                                    <i class="fas fa-plus-circle nav-icon"></i>
+                                    <p>Agregar Empresa</p>
+                                    </a>
+                                    </li>
+                                <li class="nav-item">
+                                    <a href="/#" target="frameprincipal" class="nav-link">
+                                    <i class="fas fa-list-ol nav-icon"></i>
+                                    <p>Listar Empresas</p>
+                                </a>
+                                </li>
+                            </ul>
+                <!-- Finaliza Grupo Empresas -->
+                <!-- Grupo Contribuyentes -->
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                <i class="fas fa-people-arrows"></i>
+                                <p>Contribuyentes <i class="right fas fa-angle-left"></i></p>
+                                </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="/#" target="frameprincipal" class="nav-link">
+                                <i class="fas fa-plus-circle nav-icon"></i>
+                                <p>Agregar contribuyente</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/#" target="frameprincipal" class="nav-link">
+                                <i class="fas fa-id-badge nav-icon"></i>
+                                <p>Listar Contribuyentes</p>
+                                </a>
+                            </li>
+                        </ul>
+                <!-- Finaliza Grupo Contribuyentes -->
                     </ul>
                 </li>
 
