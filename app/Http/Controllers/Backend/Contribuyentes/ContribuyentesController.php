68efeeb14e4fe1c16c66ea06b10b1ad69b11de53
vaneayala05 <vanessaayala127@gmail.com>
2022-01-11 10:06:37 -0600

    mantenimiento tabla contribuyente

--- app/Http/Controllers/Backend/Contribuyentes/ContribuyentesController.php
+++ app/Http/Controllers/Backend/Contribuyentes/ContribuyentesController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Backend\Contribuyentes;
 use App\Models\Contribuyentes;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use GrahamCampbell\ResultType\Success;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Unique;
+use Symfony\Contracts\Service\Attribute\Required;
 use function PHPUnit\Framework\isEmpty;
 
 
@@ -22,22 +25,187 @@ class ContribuyentesController extends Controller
 
     public function listarContribuyentes()
     {
-        $contribuyentes = Contribuyentes::All();
+         $contribuyentes = Contribuyentes::All();
         $infContribuyente = Contribuyentes::where('id', $contribuyentes)->first();
     //     ->join('empresa','empresa.id_empresa','=','contribuyentes.id_empresa')
     //     ->select('contribuyente.nombre','empresa.nombre')
     //     ->get();
     //     dd($contribuyentes);
+
         return view('backend.admin.Contribuyentes.ListarContribuyentes', compact('contribuyentes'));
     }
+  
+//Agregar nuevo contribuyente
+
     public function crearContribuyentes()
     {
-
         return view('backend.admin.Contribuyentes.Crear_Contribuyentes');
     }
     public function tablaContribuyentes(){
         $contribuyentes = Contribuyentes::orderBy('id', 'ASC')->get();
 
         return view('backend.admin.Contribuyentes.tabla.tablalistacontribuyentes', compact('contribuyentes'));
+    
     }
-}
+
+//Agregar nuevo contribuyente
+
+    public function nuevoContribuyente (Request $request){
+    
+        $regla = array(
+            'nombre' => 'Required',
+            'apellido' => 'Required',
+            'direccion' => 'Required',
+            'dui' => 'Required|unique:contribuyente,dui',
+            'nit' => 'Required|unique:contribuyente,nit',
+            'registro_comerciante' => 'Required|unique:contribuyente,registro_comerciante',
+            'telefono' => 'Required',
+            'email' => 'Required',
+            'fax' => 'nullable'
+          
+          );
+  
+          $validar = Validator::make($request->all(), $regla);
+  
+          
+          if ($validar->fails()){ return ['success' => 0];}
+          
+              $dato = new Contribuyentes();
+              $dato->nombre = $request->nombre;
+              $dato->apellido = $request->apellido;
+              $dato->direccion = $request->direccion;
+              $dato->dui = $request->dui;
+              $dato->nit = $request->nit;
+              $dato->registro_comerciante = $request->registro_comerciante;
+              $dato->telefono = $request->telefono;
+              $dato->email = $request->email;
+              $dato->fax = $request->fax;
+  
+              if($dato->save()){
+                  return ['success' => 1];
+              }else{
+                  return ['success' => 2];
+              }
+          }  
+//Función para llamar informacion
+
+        public function informacionContribuyentes(Request $request)
+           {
+            $regla = array(
+                'id' => 'required',
+            );
+    
+            $validar = Validator::make($request->all(), $regla);
+    
+            if ($validar->fails()){ return ['success' => 0];}
+    
+            if($lista = Contribuyentes::where('id', $request->id)->first()){
+            
+            return ['success' => 1,
+                'contribuyente' => $lista,
+               ];
+            }else{
+                return ['success' => 2];
+            }
+            }
+        
+//Funcion editar contribuyentes
+
+        public function editarContribuyente(Request $request)
+          {
+            
+            $regla = array(
+                'id' => 'required',
+                'nombre' => 'required',
+                'apellido' => 'required',
+                'direccion' => 'required',
+                'dui' => 'required',
+                'nit' => 'required',
+                'registro_comerciante' => 'required',
+                'telefono' => 'required',
+                'email' => 'required',
+                'fax' => 'nullable',
+            
+            );
+
+            $validar = Validator::make($request->all(), $regla);
+
+            if ($validar->fails()){ return ['success' => 0];} 
+            
+            if($contribuyente = Contribuyentes::where('id', $request->id)->first())
+            {
+
+                    Contribuyentes::where('id', $request->id)->update([
+                        'nombre' => $request->nombre,
+                        'apellido' => $request->apellido,
+                        'direccion' => $request->direccion,
+                        'dui' => $request->dui,
+                        'nit' => $request->nit,
+                        'registro_comerciante' => $request->registro_comerciante,
+                        'telefono' => $request->telefono,
+                        'email' => $request->email,
+                        'fax' => $request->fax,
+                       
+                    ]);
+
+                //  $contribuyente->save();
+
+                    return ['success' => 1];
+                }else {
+                    return['success' => 2];
+                }              
+         
+                //return view('backend.admin.Contribuyentes.ListarContribuyentes', compact('contribuyente'));
+            }
+         
+//Eliminar Contribuyente    
+
+        public function eliminarContribuyentes(Request $request)
+            {
+
+                // buscamos el contribuyente el cual queremos eliminar
+                $contribuyente = Contribuyentes::find($request->id);
+                $contribuyente->delete();
+             
+                return ['success' => 1];
+            }
+
+      //     public function borrarContribuyente(Request $request)
+        //    {
+          //      $contribuyente = Contribuyentes::findById($request->id);
+            //    $contribuyente->delete();
+
+              //  return ['success' => 1];
+           // }
+   
+
+           public function verinfoContribuyentes(Request $request)
+           {
+
+            $regla = array(
+                'id' => 'required',
+            );
+    
+            $validar = Validator::make($request->all(), $regla);
+    
+            if ($validar->fails()){ return ['success' => 0];}
+    
+            if($lista = Contribuyentes::where('id', $request->id)->first()){
+            
+            return ['success' => 1,
+                'contribuyente' => $lista,
+               ];
+            }else{
+                return ['success' => 2];
+            }
+        }
+
+        }
+           
+    
+      
+    
+    
+
+    
+
--- resources/views/backend/admin/Contribuyentes/ListarContribuyentes.blade.php
+++ resources/views/backend/admin/Contribuyentes/ListarContribuyentes.blade.php
@@ -34,9 +34,8 @@
             </button>
       </div>
     </section>
-
     <section class="content">
-      <div class="container-fluid">
+    <div class="container-fluid">
         <!-- CAJA -->
         <form class="form-horizontal" id="form1">
         <div class="card card-green">
@@ -47,19 +46,20 @@
               <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
               <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-remove"></i></button>
             </div>
-          </div>
-          <!-- /.card-header -->
-          <div class="card-body">
+        </div>
+    <!-- /.card-header -->
+            <div class="card-body">
             <div class="row">
             <div class="col-md-6">
             </div>
             </div>
+
+            <!-- Aqui termina-->
             <!-- /.col -->
 
             <div id="tablaDatatable"></div>
             </div>
 
-    
             </div>
           <!-- /.row -->
           </div>
@@ -75,7 +75,240 @@
       </div>
     <!-- /.container-fluid -->
     </section>
+
+ <!-- /Modal Actualizar contribuyente-->
+
+    <div class="modal fade" id="modalEditar">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Actualizar Contribuyente</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+               <div class="modal-body">
+                    <form id="formulario-editar">
+                        <div class="card-body">
+                                                          
+                <!--inicia los campos del formulario-->
+                 <!-- /.card-header -->
+                 <div class="card-body">
+                  <div class="row">
+                        <div class="col-md-6">
+                        <div class="form-group">
+                         <label>Nombre:</label>
+                        <input type="text" name="nombre" id="nombre-editar" class="form-control" required placeholder="Nombre del propietario">
+                        <input type="hidden" name="id" id="id-editar"  >
+                      </div>
+                <!-- /.form-group -->
+                <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                          <label>NIT:</label>
+                          <input type="number" name="nit" id="nit-editar" class="form-control" required placeholder="0000-000000-000-0" >
+                  </div></div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                          <label>DUI:</label>
+                          <input type="number" name="dui" id="dui-editar" required placeholder="00000000-0" class="form-control" >
+                      </div>
+                  </div>
+                </div>
+                <!-- /.form-group -->
+                <div class="form-group">
+                    <label>Dirección:</label>
+                    <input type="text" name="direccion" id="direccion-editar" class="form-control" placeholder="Dirección de la empresa">
+                  </div>
+                <!-- /.form-group --> 
+                <div class="form-group">
+                    <label>Correo Electrónico:</label>
+                    <input type="email" name="email" id="email-editar" class="form-control" placeholder="Correo@dominio.com"  >
+                  </div>
+                <!-- /.form-group -->  
+              </div>
+                <!-- Inicia Segunda Columna de campos-->
+              <!-- /.col -->
+              <div class="col-md-5">
+              <div class="form-group">
+                        <label>Apellido:</label>
+                        <input type="text" name="apellido" id="apellido-editar" class="form-control" required placeholder="Apellido del propietario" >
+                      </div>
+                <!-- /.form-group -->
+                <div class="form-group">
+                <label>Registro de Comerciante:</label>
+                          <input type="number" name="registro_comerciante-editar" id="registro_comerciante-editar"  class="form-control" required placeholder="0000000"  >
+                      </div>
+                <!-- /.form-group -->
+                <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                          <label>Teléfono:</label>
+                          <input type="number" name="telefono" id="telefono-editar" class="form-control" required placeholder="7777-7777">
+                  </div></div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                          <label>Fax:</label>
+                          <input type="number" name="fax" id="fax-editar" required placeholder="0000-0000" class="form-control" >
+                     </div>
+                   </div>
+                  </div>
+                </div>
+             </div>
+            
+              <!--finaliza los campos del formulario-->
+                     </div>
+                     </form>
+                    </div>
+                    <div class="modal-footer justify-content-between">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                        <button type="button" class="btn btn-success  float-right" onclick="actualizar()">Actualizar</button>
+                    </div>
+                   </div>
+                  </div>
+                </div>
+              </div>
+           </div>
+
+        <!--Finaliza Modal Actualizar contribuyente -->
+
+        <!-- /Modal ver datos del contribuyente-->
+
+    <div class="modal fade" id="modalVer">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Datos del contribuyente</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+               <div class="modal-body">
+                    <form id="formulario-ver">
+                        <div class="card-body">
+                                                          
+                <!--inicia los campos del formulario ver-->
+                 <!-- /.card-header -->
+                 <div class="card-body">
+                  <div class="row">
+                        <div class="col-md-6">
+                        <div class="form-group"> 
+                         <label>Nombre:</label>
+                        <input type="text" name="nombre" id="nombre-ver" disabled class="form-control" >
+                        <input type="hidden" name="id" id="id-ver"  >
+                      </div>
+                <!-- /.form-group -->
+                <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                          <label>NIT:</label>
+                          <input type="number" name="nit" id="nit-ver" disabled class="form-control" >
+                  </div></div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                          <label>DUI:</label>
+                          <input type="number" name="dui" id="dui-ver" disabled class="form-control" >
+                     </div>
+                    </div>
+                  </div>
+                <!-- /.form-group -->
+                <div class="form-group">
+                    <label>Dirección:</label>
+                    <input type="text" name="direccion" id="direccion-ver" disabled class="form-control">
+                  </div>
+                <!-- /.form-group --> 
+                <div class="form-group">
+                    <label>Correo Electrónico:</label>
+                    <input type="email" name="email" id="email-ver" disabled class="form-control" >
+                  </div>
+                <!-- /.form-group -->  
+              </div>
+                <!-- Inicia Segunda Columna de campos-->
+              <!-- /.col -->
+              <div class="col-md-5">
+              <div class="form-group">
+                        <label>Apellido:</label>
+                        <input type="text" name="apellido" id="apellido-ver" disabled class="form-control" >
+                      </div>
+                <!-- /.form-group -->
+                <div class="form-group">
+                <label>Registro de Comerciante:</label>
+                    <input type="number" name="registro_comerciante-ver" id="registro_comerciante-ver" disabled required class="form-control" >
+                      </div>
+                <!-- /.form-group -->
+                <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                          <label>Teléfono:</label>
+                          <input type="number" name="telefono" id="telefono-ver" disabled class="form-control" required >
+                  </div></div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                          <label>Fax:</label>
+                          <input type="number" name="fax" id="fax-ver" disabled required class="form-control" >
+                     </div>
+                   </div>
+                  </div>
+                </div>
+             </div>
+            
+              <!--finaliza los campos del formulario-->
+                     </div>
+                     </form>
+                    </div>
+                    <div class="modal-footer justify-content-center">
+                         <button type="button" class="btn btn-success" data-dismiss="modal">Aceptar</button>
+                    
+                    </div>
+                   </div>
+                  </div>
+                </div>
+              </div>
+           </div>
+
+        <!--Finaliza Modal ver datos del contribuyente -->
+
+ <!-- Inicia Modal Borrar Contribuyente-->
+
+ <div class="modal fade" id="modalEliminar">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Eliminar Contribuyente</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form id="formulario-borrar">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+
+                                    <p>¿Realmente desea eliminar el contribuyente seleccionado?"</p>
+
+                                    <div class="form-group">
+                                        <input type="hidden" id="idborrar">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-danger" onclick="eliminarC()">Borrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+        <!--Finaliza Modal Borrar Contribuyente-->
 </div>
+</div>
+</div>
+</div>
+
 <!--Termina Contenido Frame Principal -->
 @extends('backend.menus.footerjs')
 @section('archivos-js')
@@ -100,7 +333,177 @@
         $(document).ready(function(){
             document.getElementById("divcontenedor").style.display = "block";
         });
-    </script>
+</script>
 
+<script type="text/javascript">
 
+    function recargar()
+    {
+        var ruta = "{{ url('/admin/contribuyentes/tabla') }}";
+            $('#tablaDatatable').load(ruta);
+    }
+
+        function editarContribuyentes(id){
+        document.getElementById("formulario-editar").reset();
+            $('#modalEditar').modal('show');
+        }
+
+       function informacionContribuyentes(id){
+                  openLoading();
+                 document.getElementById("formulario-editar").reset();
+
+            axios.post('/admin/Contribuyentes/informacion',{
+                'id': id
+            })
+                .then((response) => {
+                    closeLoading();
+                    if(response.data.success === 1){
+                        $('#modalEditar').modal('show');
+
+                        $('#id-editar').val(response.data.contribuyente.id);
+                        $('#nombre-editar').val(response.data.contribuyente.nombre);
+                        $('#apellido-editar').val(response.data.contribuyente.apellido);
+                        $('#dui-editar').val(response.data.contribuyente.dui);
+                        $('#nit-editar').val(response.data.contribuyente.nit);
+                        $('#registro_comerciante-editar').val(response.data.contribuyente.registro_comerciante);
+                        $('#direccion-editar').val(response.data.contribuyente.direccion);
+                        $('#telefono-editar').val(response.data.contribuyente.telefono);
+                        $('#email-editar').val(response.data.contribuyente.email);
+                        $('#fax-editar').val(response.data.contribuyente.fax);
+                    }else{
+                        toastr.error('Información no encontrada');
+                    }
+                    
+                })
+                .catch((error) => {
+                    closeLoading();
+                    toastr.error('Información no encontrada');
+                });
+       
+            }
+
+            function actualizar()
+            {
+                 var id = document.getElementById('id-editar').value;
+                 var nombre = document.getElementById('nombre-editar').value;
+                 var apellido = document.getElementById('apellido-editar').value;
+                 var dui = document.getElementById('dui-editar').value;
+                 var nit = document.getElementById('nit-editar').value;
+                 var registro_comerciante = document.getElementById('registro_comerciante-editar').value;
+                 var direccion = document.getElementById('direccion-editar').value;
+                 var telefono = document.getElementById('telefono-editar').value;
+                 var email = document.getElementById('email-editar').value;
+                 var fax = document.getElementById('fax-editar').value;
+
+            openLoading()
+
+                var formData = new FormData();
+                formData.append('id', id);
+                formData.append('nombre', nombre);
+                formData.append('apellido', apellido);
+                formData.append('dui', dui);
+                formData.append('nit', nit);
+                formData.append('registro_comerciante', registro_comerciante);
+                formData.append('direccion', direccion);
+                formData.append('telefono', telefono);
+                formData.append('email', email);
+                formData.append('fax', fax);
+         
+            axios.post('/admin/Contribuyentes/editar', formData, {
+            })
+                .then((response) => {
+                    closeLoading()
+
+                   if (response.data.success === 1) 
+                    {
+                        toastr.success('Contribuyente actualizado');
+                    }
+                    else 
+                    {
+                        toastMensaje('Error al actualizar');
+                        $('#modalEditar').modal('hide');
+                               recargar();
+                    }
+                })
+                .catch((error) => {
+                    closeLoading()
+                    toastMensaje('error', 'Error');
+                });
+        }
+ 
+    //se recibe el ID del contribuyente a eliminar
+
+            function modalEliminar(id)
+            {
+                $('#idborrar').val(id);
+                $('#modalEliminar').modal('show');
+            }
+
+        function eliminarC(){
+            openLoading()
+        
+            // se envia el ID del contribuyente
+            var id = document.getElementById('idborrar').value;
+
+            var formData = new FormData();
+            formData.append('id', id);
+
+            axios.post('/admin/Contribuyentes/eliminar_contribuyentes', formData, {
+            })
+                .then((response) => {
+                    closeLoading()
+                    $('#modalEliminar').modal('hide');
+                    
+                    if(response.data.success === 1){
+                        toastMensaje('success', 'Contribuyente eliminado');
+                        recargar();
+                    }else{
+                        toastMensaje('error', 'Error al borrar');
+           
+                    }
+                })
+                
+                .catch(function (error) {
+                        closeLoading()
+                        toastr.error("Error de Servidor!");
+                      }); 
+        }
+
+//funcion para ver la informacion del contribuyente
+
+        function verContribuyentes(id){
+                  openLoading();
+                 document.getElementById("formulario-ver").reset();
+
+            axios.post('/admin/Contribuyentes/informacion',{
+                'id': id
+            })
+                .then((response) => {
+                    closeLoading();
+                    if(response.data.success === 1){
+                        $('#modalVer').modal('show');
+
+                        $('#id-ver').val(response.data.contribuyente.id);
+                        $('#nombre-ver').val(response.data.contribuyente.nombre);
+                        $('#apellido-ver').val(response.data.contribuyente.apellido);
+                        $('#dui-ver').val(response.data.contribuyente.dui);
+                        $('#nit-ver').val(response.data.contribuyente.nit);
+                        $('#registro_comerciante-ver').val(response.data.contribuyente.registro_comerciante);
+                        $('#direccion-ver').val(response.data.contribuyente.direccion);
+                        $('#telefono-ver').val(response.data.contribuyente.telefono);
+                        $('#email-ver').val(response.data.contribuyente.email);
+                        $('#fax-ver').val(response.data.contribuyente.fax);
+                    }else{
+                        toastr.error('Información no encontrada');
+                    }
+
+                })
+                .catch((error) => {
+                    closeLoading();
+                    toastr.error('Información no encontrada');
+                });
+       
+            }
+
+</script>
 @stop
