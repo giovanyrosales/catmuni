8eea0dcc34bb205a104d272950b64b5be32f00b0
vaneayala05 <vanessaayala127@gmail.com>
2022-01-26 10:55:49 -0600

    cambios necesarios

--- resources/views/backend/admin/TarifaFija/tabla/tablalistatarifafija.blade.php
+++ resources/views/backend/admin/TarifaFija/tabla/tablalistatarifafija.blade.php
@@ -20,9 +20,9 @@
                                 <tr>
                                     <td>{{$tarifa_fija->nombre_actividad}} </td>
                                     <td>{{$tarifa_fija->nombre_rubro}} </td>
-                                    <td>{{$tarifa_fija->limite_inferior}} </td>
-                                    <td>{{$tarifa_fija->limite_superior}} </td>
-                                    <td>{{$tarifa_fija->impuesto_mensual}} </td>
+                                    <td>${{$tarifa_fija->limite_inferior}} </td>
+                                    <td>${{$tarifa_fija->limite_superior}} </td>
+                                    <td>${{$tarifa_fija->impuesto_mensual}} </td>
                                   
                                     <td style="text-align: center;">
                                                                    
