d800ed08afeada4492c26ce8d81336d86838327f
vaneayala05 <vanessaayala127@gmail.com>
2022-02-16 08:18:44 -0600

    formulario empresa cambios

--- routes/web.php
+++ routes/web.php
@@ -151,3 +151,5 @@ use App\Http\Controllers\Backend\ActividadEspecifica\ActividadEspecificaControll
     Route::post('/admin/ActividadEspecifica/informacion', [ActividadEspecificaController::class, 'informacionActividadEspecifica']);
     Route::post('/admin/ActividadEspecifica/editar', [ActividadEspecificaController::class, 'editarActividadEspecifica']);
     Route::post('/admin/ActividadEspecifica/eliminar', [ActividadEspecificaController::class, 'eliminarActividadEspecifica']);
+
+    Route::post('/admin/empresa/buscar', [EmpresaController::class,'buscarActividadEsp'] );
