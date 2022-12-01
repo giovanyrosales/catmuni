881a2fb98e2e33444086b889d09ae3e6358086a9
thiagomancia17 <thiagomancia@gmail.com>
2022-12-01 10:10:40 -0600

    modificacion en mora tributaria periodica

--- resources/views/backend/admin/Reportes/MoraTributaria/vistaReporteMoraTributaria.blade.php
+++ resources/views/backend/admin/Reportes/MoraTributaria/vistaReporteMoraTributaria.blade.php
@@ -103,7 +103,7 @@
                                     <th style="width: 20%; text-align: center;font-weight: 700;">EMPRESA O NEGOCIO</th>       
                                     <th style="width: 15%; text-align: center;font-weight: 700;">ULTIMO PAGO</th>
                                     <th style="width: 10%; text-align: center;font-weight: 700;">MESES</th>
-                                    <th style="width: 15%; text-align: center;font-weight: 700;">ULTIMA TARIFA/AÑO</th>
+                                    <th style="width: 15%; text-align: center;font-weight: 700;">TARIFA/AÑO</th>
                                     <th style="width: 12%; text-align: right;font-weight: 700;">MORA</th>
                                 </tr>
                             </thead>
@@ -292,7 +292,7 @@
 
         function generarPdfMoraTributaria(){
 
-            window.open("{{ URL::to('admin/pdf/reporte/mora_tributaria') }}/");
+            window.open("{{ URL::to('admin/pdf/reporte/mora_tributaria_global') }}/");
        
         }
 
